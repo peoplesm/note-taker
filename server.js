9bdda9efd603d72a7e47abf80403a29f6ebfe54e
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-09-02 11:20:05 -0600

    removed redundant code from server.js

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ app.use("/api", api);
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => res.sendFile("/public/index.html"));
-
 // GET Route for notes page
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
